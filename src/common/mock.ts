f3b81afdf1ef3b76b4b4ecb2ec24d5891ff014c4
anig1scur <anigiscur@gmail.com>
2024-08-17 18:52:30 +0800

    mock

---
+++ src/common/mock.ts
@@ -0,0 +1,222 @@
+const data = [
+  {
+    "key": {
+      "en": "Sichuaner",
+      "cn": "四川人"
+    },
+    "desc": {
+      "cn": "我出生成长于四川东北的一个小镇。",
+      "en": "I was born and raised in a small town in northeastern Sichuan."
+    }
+  },
+  {
+    "key": {
+      "en": "Spicy & Sour",
+      "cn": "酸辣"
+    },
+    "desc": {
+      "cn": "我最喜欢的食物的口味是酸辣。两者都有是最好，但仅有其一也未尝不可。",
+      "en": "My favorite food flavors are spicy and sour. Having both is best, but either one alone is also good. I like dishes like Suan La Fen, Luosifen, and Hunan cuisine."
+    }
+  },
+  {
+    "key": {
+      "en": "Non-talkative",
+      "cn": "不善言谈"
+    },
+    "desc": {
+      "cn": "我不太擅长与人交流，更喜欢独处做些自己的事情。",
+      "en": "I'm not very good at communicating with others. I prefer to be alone and do my own things."
+    }
+  },
+  {
+    "key": {
+      "en": "Thunderstorm",
+      "cn": "雷雨"
+    },
+    "desc": {
+      "cn": "蓝天白云很好，但我也喜欢雷雨天的夜里放些 jazz 音乐时给人意乱情迷的感觉。",
+      "en": "Clear blue skies are nice, but I also love the intoxicating feeling of listening to jazz music on a stormy night."
+    }
+  },
+  {
+    "key": {
+      "en": "MBTI",
+      "cn": "MBTI"
+    },
+    "desc": {
+      "cn": "没那么 t 的 intp ，甚至越来越不 t ，逐渐地我开始喜欢大哭的感觉！",
+      "en": "I'm an INTP, but not so much 'T'. I'm becoming less 'T' over time, and I'm starting to enjoy the feeling of crying!"
+    }
+  },
+  {
+    "key": {
+      "en": "Emotional",
+      "cn": "情绪化"
+    },
+    "desc": {
+      "cn": "一个人的时候很爱哭，我把这个当作自己演技的一种证明。",
+      "en": "I cry a lot when I'm alone. I consider this as proof of my acting skills."
+    }
+  },
+  {
+    "key": {
+      "en": "400 Degrees",
+      "cn": "400度"
+    },
+    "desc": {
+      "cn": "我的左眼近视 400 度，右眼比这个更高。",
+      "en": "My left eye is 400 degrees nearsighted, and my right eye is even worse."
+    }
+  },
+  {
+    "key": {
+      "en": "Web Developer",
+      "cn": "Web 开发者"
+    },
+    "desc": {
+      "cn": "我是一个 Web 开发者，我以此为生。",
+      "en": "I am a web developer. This is how I make a living."
+    }
+  },
+  {
+    "key": {
+      "en": "Dyslexia",
+      "cn": "失读症"
+    },
+    "desc": {
+      "cn": "我不是真的有失读症，只是很难读下长文字。我把它视为我的工作习惯对我的一种毒害。",
+      "en": "I don't really have dyslexia, I just find it hard to read long texts. I see this as a toxic effect of my work habits on me."
+    }
+  },
+  {
+    "key": {
+      "en": "Pop Music",
+      "cn": "流行乐"
+    },
+    "desc": {
+      "cn": "我听歌的审美非常大众：华语流行，其它语流行。Spotify 给我的 tag 是 pop, pop, pop",
+      "en": "My music taste is very mainstream: Chinese pop, and pop in other languages. Spotify tags me as 'pop, pop, pop'."
+    }
+  },
+  {
+    "key": {
+      "en": "Sibling",
+      "cn": "兄弟姐妹"
+    },
+    "desc": {
+      "cn": "我有一个姐姐，还有一个弟弟。",
+      "en": "I have an older sister and a younger brother."
+    }
+  },
+  {
+    "key": {
+      "en": "UI Starter",
+      "cn": "UI 入门者"
+    },
+    "desc": {
+      "cn": "我曾经跟随我的设计师朋友学习过一段时间，现在我做网页时会提前使用 figma 简单画画。",
+      "en": "I used to learn from my designer friend for a while. Now I use Figma to sketch simple designs before I start working on websites."
+    }
+  },
+  {
+    "key": {
+      "en": "UX Fans",
+      "cn": "UX 爱好者"
+    },
+    "desc": {
+      "cn": "我在浏览网页 / 使用 App 时会留意它给人的感受，并评判这个设计的好坏。",
+      "en": "When browsing websites or using apps, I pay attention to how they make people feel and judge whether the design is good or bad."
+    }
+  },
+  {
+    "key": {
+      "en": "No Bra",
+      "cn": "不穿内衣"
+    },
+    "desc": {
+      "cn": "从 Covid 开始就不穿内衣啦！",
+      "en": "I've stopped wearing bras since Covid started!"
+    }
+  },
+  {
+    "key": {
+      "en": "Gen Z",
+      "cn": "Z 世代"
+    },
+    "desc": {
+      "cn": "我出生在千禧年中旬。我很开心我出生在这个时间。",
+      "en": "I was born in the mid-2000. I'm happy to have been born at this time."
+    }
+  },
+  {
+    "key": {
+      "en": "Stoicism",
+      "cn": "斯多葛主义"
+    },
+    "desc": {
+      "cn": "我是斯多葛主义拥护者：悲观主义，积极生活。我认为它给了我最大限度的人生弹性。",
+      "en": "I'm a supporter of Stoicism: pessimistic in thought, positive in action. I believe it gives me maximum flexibility in life."
+    }
+  },
+  {
+    "key": {
+      "en": "Experience Junkie",
+      "cn": "体验狂热者"
+    },
+    "desc": {
+      "cn": "我是一个体验至上主义者。去某一个地方旅游我会倾向寻找一些当地特殊的体验活动，而不是著名景点打卡。",
+      "en": "I prioritize experiences. When traveling, I tend to seek out unique local experiences rather than checking off famous tourist spots."
+    }
+  },
+  {
+    "key": {
+      "en": "Blender",
+      "cn": "Blender"
+    },
+    "desc": {
+      "cn": "最近我的学习目标是 blender，最终我希望做一个完整的 3D 应用。",
+      "en": "My recent learning goal is Blender. Ultimately, I hope to create a complete 3D application."
+    }
+  },
+  {
+    "key": {
+      "en": "Heterosexuality",
+      "cn": "异性恋"
+    },
+    "desc": {
+      "cn": "到目前为止，我的认知里我是一个异性恋。",
+      "en": "As far as I know, I am heterosexual."
+    }
+  },
+  {
+    "key": {
+      "en": "DIVA Cup",
+      "cn": "月经杯"
+    },
+    "desc": {
+      "cn": "近期我开始使用月经杯，我认为它解放了经期中的女性！",
+      "en": "I recently started using a menstrual cup. I think it liberates women during their periods!"
+    }
+  },
+  {
+    "key": {
+      "en": "Shameless",
+      "cn": "无耻之徒"
+    },
+    "desc": {
+      "cn": "我曾花很长的时间追完所有季的 《无耻之徒》，它教会我一种打不死的不肯屈服的精神。",
+      "en": "I once spent a long time watching all seasons of 'Shameless'. It taught me an indomitable spirit that refuses to give up."
+    }
+  },
+  {
+    "key": {
+      "en": "South America",
+      "cn": "南美洲"
+    },
+    "desc": {
+      "cn": "现在最想去的旅行地是南美，想去看加拉帕戈斯群岛的企鹅。",
+      "en": "The place I most want to travel to now is South America. I want to see the penguins in the Galapagos Islands."
+    }
+  }
+]
